7454abe7b2ed6ef220049fa09b73b7a3f0f00653
_AU <ashish.upadhyay@salesforce.com>
2017-07-11 01:43:56 -0700

    updating code with retyr

--- brackets.rb
+++ brackets.rb
@@ -1,30 +1,20 @@
 # @param {Integer} n
 # @return {String[]}
 def generate_parenthesis(n)
-  r = []
-  generate_one_by_one(r, '', n, n)
-  r
-end
 
-def generate_one_by_one(result, temp, left, right)
-
-    puts "result :  \t #{result}"
-    puts "temp :    \t #{temp}"
-    puts "left :    \t #{left}"
-    puts "right :   \t #{right}"
-    p '*'*30
-
-  if(left == 0 && right == 0)
-    result << temp
-    puts 'put it in'
-    p result
-    p temp
-    p ':'*100
-    return
-  end
-
-  generate_one_by_one( result, temp + "(" , left-1, right) if left > 0
-  generate_one_by_one( result, temp + ")" , left, right-1) if right > left
+  helper('', n , n, [])
 
 end
-p generate_parenthesis(2)
+
+def helper(str, left , right, results)
+
+    if left == 0 && right == 0
+        results << str.dup
+    end
+
+    helper(str + '(', left-1, right, results) if left >= 0
+    helper(str + ')', left, right - 1, results) if right > left
+
+    results
+end
+p generate_parenthesis(3)
