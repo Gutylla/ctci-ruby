876da5576165b894b587e83b049ec08fd8cd44bd
_AU <ashish.upadhyay@salesforce.com>
2017-07-11 20:36:31 -0700

    fix

--- digit_string.rb
+++ digit_string.rb
@@ -1,21 +1,31 @@
 # Given the alphabet encoded as numbers (e.g., a=1, b   =2, ..., z=26),
 # and a sequence of numbers (e.g., "23413259802"), how many strings can be generated.
 
-def count(sequence)
-  sum = 0
-  i = 1
+def count(message)
+  return 0 if message.empty?
+  total      = 1
+  n          = message.size
+  prev_total = total
+  i          = n-1
 
-  if (sequence.to_s.length > 0 && sequence.to_i < 27)
-    sum = 1
+  while i >= 0
+
+    current_char    = message[i].to_i
+    adjact_char     = message[i,2].to_i
+
+    current_count   = (current_char == 0) ? 0 : total
+
+    current_count  += prev_total if i < n-1 && current_char > 0 && adjact_char < 27
+
+    prev_total      = total
+    total           = current_count
+
+    i -= 1
   end
 
-  while (i < sequence.to_s.length)
-    sum = sum + count(sequence.to_s[i,sequence.to_s.length])
-    i += 1
-  end
-  sum
+  message.nil? ? 0 : total
 end
 
-p count(121)
+p count("102110")
 # p count(23413259802)
 # p count(1221)
